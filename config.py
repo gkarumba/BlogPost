8fc58cb26a7ef9e2cc320b1e7812f2b5fa2c660b
gkarumba <gachegua@gmail.com>
2019-07-06 17:46:29 +0300

    Add the auth initialization

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:123456@localhost/postblog'
